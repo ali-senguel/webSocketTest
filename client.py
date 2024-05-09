e47802a28271c76f8d8132d3a7a8a6b2fd37a229
ali-senguel <ali.senguel@fb.com>
2024-05-09 23:32:16 +0200

    Updated webSocket server directory Readme and gitignore

--- client.py
+++ client.py
@@ -6,7 +6,8 @@ import websockets
 async def test():
     #async with websockets.connect('ws://192.168.1.100:8069') as websocket:
     async with websockets.connect('ws://localhost:8000') as websocket:
-        await websocket.send("hello")
+        next_step = input("Type next step i.e Move or Continue")
+        await websocket.send(next_step)
         response = await websocket.recv()
         print(response)
         #cmd = 'jack_disconnect genltc:ltc system:playback_1'
